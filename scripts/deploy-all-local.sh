0b97884495dba53c282ff4cee28ee9db74eeffa0
Tiago Loureiro <19423709+tiagoicp@users.noreply.github.com>
2024-03-21 07:00:20 +0000

    added local script and fixed image issue

---
+++ scripts/deploy-all-local.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# REQUIREMENTS
+# - on another tab you already did "dfx start" (--clean)
+# - you have a parent folder (like "obsidian")
+# - and the children are all the dependent repos (like game, hero, item, frontend/backend).
+
+# SCRIPT SUMMARY
+# - go through all folders / repo
+# - on each, do dfx deploy
+
+dfx deploy
+cd ..
+cd obsidian_tears_hero_nft
+dfx deploy
+cd ..
+cd obsidian_tears_items_nft
+dfx deploy
+cd ..
+cd obsidian_tears_game/Webserver
+dfx deploy
+
+echo ""
+echo "Finished Deploy Local Script"
+echo ""

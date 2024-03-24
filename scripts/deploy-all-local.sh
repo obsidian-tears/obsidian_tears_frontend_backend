5c9274148da6f86a029b138875d065235706c3bd
Tiago Loureiro <19423709+tiagoicp@users.noreply.github.com>
2024-03-24 17:37:40 +0000

    added mint dev nfts to local script

--- scripts/deploy-all-local.sh
+++ scripts/deploy-all-local.sh
@@ -13,6 +13,8 @@ dfx deploy
 cd ..
 cd obsidian_tears_hero_nft
 dfx deploy
+dfx canister call obsidian_tears_nft _mintAndTransferDevHero $(dfx identity get-principal)
+dfx canister call obsidian_tears_nft _mintAndTransferDevHero $(dfx identity get-principal)
 cd ..
 cd obsidian_tears_items_nft
 dfx deploy

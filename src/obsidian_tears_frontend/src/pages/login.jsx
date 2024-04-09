0826eea3046ccb466bc5ad95c7cdf80baddc8e0e
0xdorifto <gastaosousafaria@gmail.com>
2024-04-09 10:43:46 +0100

    Compress states and add stoic provider

--- src/obsidian_tears_frontend/src/pages/login.jsx
+++ src/obsidian_tears_frontend/src/pages/login.jsx
@@ -1,8 +1,14 @@
+import { Actor } from "@dfinity/agent";
 import PlugConnect from "@psychedelic/plug-connect";
 import * as React from "react";
+import { obsidian_tears_backend as backendActor } from "../../../declarations/obsidian_tears_backend";
 import Navbar from "../components/navbar";
+import { characterCanisterId, itemCanisterId } from "../env";
+import { connectToStoic } from "../providers/stoicProvider";
 
 const Login = (props) => {
+  const gameCanisterId = Actor.canisterIdOf(backendActor);
+  const whitelist = [gameCanisterId, itemCanisterId, characterCanisterId];
   // asset urls
   const backgroundImageWood3 = { backgroundImage: "url(button-wood-3.png)" };
 
@@ -11,7 +17,7 @@ const Login = (props) => {
       id="body"
       style={{ backgroundImage: "url(background-large-obelisk.jpg)" }}
     >
-      <Navbar loggedInWith={props.loggedInWith} logout={props.logout} />
+      <Navbar />
 
       <div>
         <img src="menu-big-logo.png" alt="menu logo"></img>
@@ -20,7 +26,7 @@ const Login = (props) => {
           {false && (
             <div className="centerMe">
               <PlugConnect
-                whitelist={props.whitelist}
+                whitelist={whitelist}
                 onConnectCallback={props.connectToPlug}
               />
               <br></br>
@@ -31,7 +37,14 @@ const Login = (props) => {
               className={"buttonWoodGridXL"}
               style={backgroundImageWood3}
               onClick={async () => {
-                await props.connectToStoic();
+                await connectToStoic(
+                  props.identity,
+                  props.setLoginInfo,
+                  props.setRoute,
+                  props.setGameActor,
+                  props.setCharActor,
+                  props.setItemActor
+                );
               }}
             >
               Connect to Stoic
---
+++ src/obsidian_tears_frontend/src/providers/stoicProvider.js
@@ -0,0 +1,82 @@
+import { Actor, HttpAgent } from "@dfinity/agent";
+import { StoicIdentity } from "ic-stoic-identity";
+import {
+  canisterId as backendCanisterId,
+  createActor as backendCreateActor,
+} from "../../../declarations/obsidian_tears_backend";
+import { characterIdlFactory } from "../../idl_factories/characterIdlFactory.did";
+import { itemIdlFactory } from "../../idl_factories/itemIdlFactory.did";
+import { characterCanisterId, itemCanisterId, network } from "../env";
+
+export const loadStoicActors = (
+  agent,
+  setGameActor,
+  setCharActor,
+  setItemActor
+) => {
+  let characterActor = Actor.createActor(characterIdlFactory, {
+    agent: agent,
+    canisterId: characterCanisterId,
+  });
+  setGameActor(backendCreateActor(backendCanisterId, { agent: agent }));
+  setCharActor(characterActor);
+  setItemActor(
+    Actor.createActor(itemIdlFactory, {
+      agent: agent,
+      canisterId: itemCanisterId,
+    })
+  );
+
+  return characterActor;
+};
+
+export const verifyStoicConnectionAndAgent = (
+  identity,
+  setLoginInfo,
+  setRoute
+) => {
+  StoicIdentity.load().then(async (id) => {
+    if (id) {
+      setLoginInfo((prevState) => ({
+        ...prevState,
+        principal: identity.getPrincipal(),
+      }));
+      let agent = new HttpAgent({ identity, host });
+      if (network === "local") {
+        agent.fetchRootKey();
+      }
+    } else {
+      let id = await StoicIdentity.connect();
+      setLoginInfo((prevState) => ({
+        ...prevState,
+        identity: identity,
+      }));
+      if (id) setRoute("nftSelector");
+    }
+  });
+};
+
+//add missing arguments
+export const connectToStoic = async (
+  identity,
+  setLoginInfo,
+  setRoute,
+  setGameActor,
+  setCharActor,
+  setItemActor
+) => {
+  StoicIdentity.load().then(async (identity) => {
+    identity = await StoicIdentity.connect();
+    let agent = new HttpAgent({ identity: identity });
+    if (network === "local") {
+      agent.fetchRootKey();
+    }
+    setLoginInfo({
+      loggedInWith: "stoic",
+      identity: identity,
+      principal: identity.getPrincipal().toText(),
+    });
+    await loadStoicActors(agent, setGameActor, setCharActor, setItemActor);
+    setRoute("nftSelector");
+  });
+};

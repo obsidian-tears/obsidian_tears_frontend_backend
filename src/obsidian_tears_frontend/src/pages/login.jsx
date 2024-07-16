d7450eebc694a0e900027dc745bb003198f4c019
Henrique Costa <102920241+hmcostaa@users.noreply.github.com>
2024-07-16 22:16:51 +0100

    fix: add policy to enable youtube videos (#51)
    Co-authored-by: Tiago Loureiro <19423709+tiagoicp@users.noreply.github.com>

--- src/obsidian_tears_frontend/src/pages/login.jsx
+++ src/obsidian_tears_frontend/src/pages/login.jsx
@@ -84,10 +84,10 @@ const Login = (props) => {
               height="315"
               src="https://www.youtube.com/embed/kyqpSycLASY?si=wUeIqm6ieZkbgGEr&amp;start=7"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
             ></iframe>
           </div>
         </div>

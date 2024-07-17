c972879e8280ba0cbe6cc4139ccc29a4d44ca16a
Henrique Costa <hmcosta2002@gmail.com>
2024-07-17 04:12:19 +0100

    feat: setup eslint in the project, add to husky pre-commit and github actions

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    ignores: ["node_modules/", ".dfx/", "dist/", "build/", "src/declarations/"],
+  },
+];
